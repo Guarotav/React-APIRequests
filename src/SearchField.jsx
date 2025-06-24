71c05ae016f4d22f0db2777316699b9b4dcf3f92
Guarotav <guarotav@gmail.com>
2025-06-24 18:48:12 -0400

    Updated Code

--- src/SearchField.jsx
+++ src/SearchField.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
-const SearchField = () => {
-  return <div></div>;
+const SearchField = ({input, setInput, handleSubmit }) => {
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Search..."
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+    </div>
+  );
 };
 
 export default SearchField;
