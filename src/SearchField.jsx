db56cd1480d993114de43e07992371335cffe392
Guarotav <guarotav@gmail.com>
2025-06-24 14:46:25 -0400

    Intial commit

--- src/SearchField.jsx
+++ src/SearchField.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
-const SearchField = () => {
-  return <div></div>;
+
+const SearchField = (props) => {
+  return <div>
+    <form onSubmit  = {handleSubmit} >
+    <input type  = "text"
+    placeholder = "Search..."
+    value = {input}
+    onChange = {(e) => props.onSearch(e)}/>
+    <button type = "submit" >Search</button>
+    </form>
+  </div>;
 };
 
 export default SearchField;
