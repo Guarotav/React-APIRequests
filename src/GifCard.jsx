a1f4ae2550fd58221ef11580cc9b0062356b8038
Guarotav <guarotav@gmail.com>
2025-06-24 19:43:27 -0400

    Change to gid format

--- src/GifCard.jsx
+++ src/GifCard.jsx
@@ -3,18 +3,11 @@ import React from "react";
 const GifCard = (props) => {
   const gif = props.gif;
 
-  const cardStyle = {
-    width: "200px",
-    borderRadius: "8px",
-    display: "inline-block",
-    margin: "8px", 
-  };
 
   return (
     <div className="gif-card">
       <img
         src={gif.images.fixed_height.url}
-        style={cardStyle}
       />
     </div>
   );
